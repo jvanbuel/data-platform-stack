b17de693d3c4d1ac011de4c85b4759c43a7b0f1a
CasperTeirlinck <casperteirlinck@gmail.com>
2025-06-12 14:00:39 +0200

    back to s3 state

--- infra/upcloud-platform/bootstrap/provider.tf
+++ infra/upcloud-platform/bootstrap/provider.tf
@@ -6,3 +6,17 @@ terraform {
     }
   }
 }
+
+# terraform {
+#   required_providers {
+#     upcloud = {
+#       source  = "UpCloudLtd/upcloud"
+#       version = "5.22.1"
+#     }
+#   }
+# }
+
+# provider "upcloud" {
+#   username = var.upcloud_username
+#   password = var.upcloud_password
+# }
