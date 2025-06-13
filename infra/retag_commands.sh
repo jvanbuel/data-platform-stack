066b9c5a57f710143c298c62ad045536d1b2e4c6
Jos Teunissen <jos.teunissen@dataminded.com>
2025-06-13 12:12:01 +0200

    add registry

---
+++ infra/retag_commands.sh
@@ -0,0 +1,3 @@
+docker pull nilli9990/dbt-exoscale
+docker tag nilli9990/dbt-exoscale rg.nl-ams.scw.cloud/funcscwdatamindedcontainerna1dvucte/dbt-scaleway:latest
+docker push rg.nl-ams.scw.cloud/funcscwdatamindedcontainerna1dvucte/dbt-scaleway:latest
