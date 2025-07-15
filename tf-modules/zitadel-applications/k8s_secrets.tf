698da63f125dc637f3e3d05eab3e39e8c401bfca
nielsc <niels.claeys@gmail.com>
2025-07-15 18:52:51 +0200

    Fix trino authentication

--- tf-modules/zitadel-applications/k8s_secrets.tf
+++ tf-modules/zitadel-applications/k8s_secrets.tf
@@ -13,6 +13,21 @@ resource "kubernetes_secret" "trino_oidc" {
   type = "Opaque"
 }
 
+#TODO rename these credentials for both next secrets: they are basically the client_credentials to talk to lakekeeper
+resource "kubernetes_secret" "trino_lakekeeper_oidc" {
+  metadata {
+    name      = "lakekeeper-credentials"
+    namespace = "services"
+  }
+
+  data = {
+    CLIENT_ID     = zitadel_machine_user.trino-opa.client_id
+    CLIENT_SECRET = zitadel_machine_user.trino-opa.client_secret
+  }
+
+  type = "Opaque"
+}
+
 resource "kubernetes_secret" "opa_oidc" {
   metadata {
     name      = "opa-credentials"
@@ -27,20 +42,6 @@ resource "kubernetes_secret" "opa_oidc" {
   type = "Opaque"
 }
 
-resource "kubernetes_secret" "lakekeeper_oidc" {
-  metadata {
-    name      = "lakekeeper-oidc"
-    namespace = "services"
-  }
-
-  data = {
-    CLIENT_ID     = zitadel_application_oidc.lakekeeper.client_id
-    CLIENT_SECRET = zitadel_application_oidc.lakekeeper.client_secret
-
-  }
-
-  type = "Opaque"
-}
 
 resource "kubernetes_secret" "lakekeeper_ui_oidc" {
   metadata {
